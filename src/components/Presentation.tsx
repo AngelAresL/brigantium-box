691378fa0810a0dfeb3fecfef2868ec416b1bfe0
Angel Ares Lago <a.areslago@gmail.com>
2024-06-06 18:39:03 +0200

    style presentation

--- src/components/Presentation.tsx
+++ src/components/Presentation.tsx
@@ -24,7 +24,7 @@ const Presentation = () => {
         <div className="mx-auto max-w-6xl text-center">
           <div className="flex flex-col justify-center items-center">
             <motion.h1
-              className="text-opacity-75 font-extrabold text-blue-300 text-outline text-title m-10"
+              className="text-opacity-75 font-extrabold text-blue-300 text-outline text-title"
               variants={containerVariants}
               initial="hidden"
               animate="visible"
@@ -38,15 +38,6 @@ const Presentation = () => {
           </div>
         </div>
       </div>
-      {/* <div className=" bg-zinc-950">
-      <p className="px-2 text-sm text-justify text-zinc-300  max-w-2xl">
-              Si estás visitando nuestra página, algo en ti ya ha cambiado.
-              Quizás estás aburrido del gimnasio, quieres darle un empujón a tu
-              preparación física, o simplemente te apetece cambiar de aires y
-              conocer gente. Pues bien, estás en el sitio indicado. ¡Esta es tu
-              mejor opción en La Coruña!
-            </p>
-      </div> */}
     </>
   );
 };
