1a57a47ece1a47bce77a46a3cfe4aeebe920386a
Angel Ares Lago <a.areslago@gmail.com>
2024-06-13 01:51:48 +0200

    general reviews

--- src/app/comunidad/page.tsx
+++ src/app/comunidad/page.tsx
@@ -4,9 +4,9 @@ import PhotoGallery from "../../components/PhotoGallery";
 
 const GalleryPage = () => {
   return (
-    <div className=" mx-auto  px-6 pt-32 pb-8 ">
-      <h1 className="text-4xl font-bold text-center">Fotos</h1>
-      <p className="text-xl text-center mt-2">
+    <div className=" mx-auto  px-6 pt-32 pb-8 bg-gradient-to-b from-zinc-900 to-zinc-950 ">
+      <h1 className="text-4xl text-zinc-400 font-bold text-center">Fotos</h1>
+      <p className="text-xl text-zinc-400 text-center mt-2">
         Aquí se viene a disfrutar y entrenar duro. Esta es nuestra familia.
       </p>
       <div className="m-8"  >
