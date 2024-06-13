499bdca1041315d05b64dde74bf58bd3cf018c5d
Luis Díaz <122988196+luisvzq@users.noreply.github.com>
2024-06-13 17:53:50 +0200

    rename jpg extensions

--- src/data/facilityImages.ts
+++ src/data/facilityImages.ts
@@ -4,16 +4,16 @@ type FacilityImage = {
 };
 const facilityImages: FacilityImage[] = [
   { src: "/inst-1.jpg", alt: "foto instalacion 1" },
-  { src: "/inst-2.jpeg", alt: "foto instalacion 2" },
-  { src: "/inst-3.jpeg", alt: "foto instalacion 3" },
-  { src: "/inst-4.jpeg", alt: "foto instalacion 4" },
+  { src: "/inst-2.jpg", alt: "foto instalacion 2" },
+  { src: "/inst-3.jpg", alt: "foto instalacion 3" },
+  { src: "/inst-4.jpg", alt: "foto instalacion 4" },
   { src: "/inst-5.jpg", alt: "foto instalacion 5" },
-  { src: "/inst-6.jpeg", alt: "foto instalacion 6" },
-  { src: "/inst-7.jpeg", alt: "foto instalacion 7" },
+  { src: "/inst-6.jpg", alt: "foto instalacion 6" },
+  { src: "/inst-7.jpg", alt: "foto instalacion 7" },
   { src: "/inst-8.jpg", alt: "foto instalacion 8" },
-  { src: "/inst-9.jpeg", alt: "foto instalacion 9" },
-  { src: "/inst-11.jpeg", alt: "foto instalacion 11" },
-  { src: "/inst-12.jpeg", alt: "foto instalacion 12" },
-  { src: "/inst-13.jpeg", alt: "foto instalacion 13" },
+  { src: "/inst-9.jpg", alt: "foto instalacion 9" },
+  { src: "/inst-11.jpg", alt: "foto instalacion 11" },
+  { src: "/inst-12.jpg", alt: "foto instalacion 12" },
+  { src: "/inst-13.jpg", alt: "foto instalacion 13" },
 ];
 export default facilityImages;
--- src/data/team.ts
+++ src/data/team.ts
@@ -10,13 +10,13 @@ const team: Coach[] = [
     name: "Oscar Rial",
     role: "First Coach & Dueño",
     bio: "Oscar Rial es el dueño de la organización y el primer coach del equipo. Es un apasionado de los videojuegos y ha dedicado su vida a los deportes electrónicos. Su objetivo es llevar a su equipo a lo más alto de la competición.",
-    imageUrl: "/oki.jpeg",
+    imageUrl: "/oki.jpg",
   },
   {
     name: "Jorge Cocoy",
     role: "Second Coach",
     bio: "Jorge Cocoy es el segundo entrenador del equipo. Es un apasionado de los videojuegos y ha dedicado su vida a los deportes electrónicos. Su objetivo es llevar a su equipo a lo más alto de la competición.",
-    imageUrl: "/cocoy.jpeg",
+    imageUrl: "/cocoy.jpg",
   },
 ];
 export default team;
